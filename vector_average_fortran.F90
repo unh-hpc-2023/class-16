f59624527bc284d6b97c11dd75e8a6d64001eb51
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-03-22 23:49:37 -0400

    class15s: parallelize vector_average in Fortran
    Unfortunately, this actually segfaults :(

--- vector_average_fortran.F90
+++ vector_average_fortran.F90
@@ -9,8 +9,10 @@ subroutine vector_average(cc, nc, n)
 
   integer :: i
 
+  !$omp parallel do
   do i = 0, n-1
      cc(i) = .5 * (nc(i) + nc(i+1))
   end do
+  !$omp end parallel do
 
 end subroutine vector_average
