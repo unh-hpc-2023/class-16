af102deb12397ed1b40da154aa1ee3b72896b070
Kai Germaschewski <kai.germaschewski@unh.edu>
2023-03-22 23:23:40 -0400

    class15: do the actual averaging

--- test_average.cxx
+++ test_average.cxx
@@ -22,6 +22,17 @@ xt::xtensor<double, 1> make_func_nc()
   return f_nc;
 }
 
+xt::xtensor<double, 1> avg(const xt::xtensor<double, 1>& f_nc)
+{
+  auto f_cc = xt::empty<double>({N});
+
+  for (int i = 0; i < N; i++) {
+    f_cc(i) = .5 * (f_nc(i) + f_nc(i + 1));
+  }
+
+  return f_cc;
+}
+
 void write_nc(const xt::xtensor<double, 1>& f, const std::string& filename)
 {
   std::ofstream of(filename);
@@ -30,8 +41,19 @@ void write_nc(const xt::xtensor<double, 1>& f, const std::string& filename)
   }
 }
 
+void write_cc(const xt::xtensor<double, 1>& f, const std::string& filename)
+{
+  std::ofstream of(filename);
+  for (int i = 0; i < f.shape(0); i++) {
+    of << (i + .5) * dx << "\t" << f(i) << "\n";
+  }
+}
+
 int main(int argc, char** argv)
 {
   auto f_nc = make_func_nc();
   write_nc(f_nc, "f_nc.asc");
+
+  auto f_cc = avg(f_nc);
+  write_cc(f_cc, "f_cc.asc");
 }
